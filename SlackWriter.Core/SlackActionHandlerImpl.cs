9c77ccda5d6d0cdf93e4231b95445dfa3a23017a
Alex Barke <alex.barke@csintelligence.asia>
2020-05-04 17:11:02 +0800

    remove default constructor for SlackClient

--- SlackWriter.Core/SlackActionHandlerImpl.cs
+++ SlackWriter.Core/SlackActionHandlerImpl.cs
@@ -2,23 +2,27 @@ using System;
 using RestSharp;
 using SlackWriter.Core.Models;
 
-namespace SlackWriter {
-    public sealed class SlackActionHandlerImpl : ISlackActionHandler {
-        private readonly Uri host = new Uri ("https://hooks.slack.com");
+namespace SlackWriter
+{
+  public sealed class SlackActionHandlerImpl : ISlackActionHandler
+  {
+    private readonly Uri host = new Uri("https://hooks.slack.com");
 
-        public SlackResponse PostMessageToChannel (string webhookSecret, string message) {
-            string path = $"services/{webhookSecret}";
+    public SlackResponse PostMessageToChannel(string webhookSecret, string message)
+    {
+      string path = $"services/{webhookSecret}";
 
-            RestClient client = new RestClient (host);
-            RestRequest request = new RestRequest (path, Method.POST, DataFormat.Json);
+      RestClient client = new RestClient(host);
+      RestRequest request = new RestRequest(path, Method.POST, DataFormat.Json);
 
-            request.AddJsonBody (new { text = message });
+      request.AddJsonBody(new { text = message });
 
-            IRestResponse response = client.Execute (request);
-            if (response.IsSuccessful) {
-                return new SlackResponse (response.Content, true, response.StatusCode);
-            }
-            return new SlackResponse ("", false, response.StatusCode);
-        }
+      IRestResponse response = client.Execute(request);
+      if (response.IsSuccessful)
+      {
+        return new SlackResponse(response.Content, true, response.StatusCode);
+      }
+      return new SlackResponse("", false, response.StatusCode);
     }
+  }
 }
--- SlackWriter.Core/SlackClient.cs
+++ SlackWriter.Core/SlackClient.cs
@@ -1,16 +1,17 @@
 using SlackWriter.Core.Models;
 
-namespace SlackWriter {
-    public class SlackClient {
-        private readonly ISlackActionHandler _actionHandler;
-        public SlackClient () {
-            _actionHandler = new SlackActionHandlerImpl ();
-        }
-        public SlackClient (ISlackActionHandler actionHandler) {
-            _actionHandler = actionHandler;
-        }
-        public SlackResponse WriteToChannel (string webhookSecret, string message) {
-            return _actionHandler.PostMessageToChannel (webhookSecret, message);
-        }
+namespace SlackWriter
+{
+  public class SlackClient
+  {
+    private readonly ISlackActionHandler _actionHandler;
+    public SlackClient(ISlackActionHandler actionHandler)
+    {
+      _actionHandler = actionHandler;
     }
+    public SlackResponse WriteToChannel(string webhookSecret, string message)
+    {
+      return _actionHandler.PostMessageToChannel(webhookSecret, message);
+    }
+  }
 }
