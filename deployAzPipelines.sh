f7e78deff48056be507dc16fcf6e9efaee1976f0
Alex Barke <alexbarke002@gmail.com>
2020-05-04 16:08:54 +0800

    Delete deployAzPipelines.sh

--- deployAzPipelines.sh
+++
@@ -1,3 +0,0 @@
- # go to the output dir for the nuget package and push it to nuget
- cd /home/vsts/work/1/a &&
- dotnet nuget push SlackWriter.$(packageVersion).nupkg -k $(NugetApiKey) -s https://api.nuget.org/v3/index.json --skip-duplicate
