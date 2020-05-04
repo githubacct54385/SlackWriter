de21a61f21b32041fccdbc7329d3e830ac9a6aa4
Alex Barke <alexbarke002@gmail.com>
2020-05-04 14:29:23 +0800

    Update deployAzPipelines.sh

--- deployAzPipelines.sh
+++ deployAzPipelines.sh
@@ -1,2 +1,3 @@
-cd /home/vsts/work/1/s/SlackWriter.Core/bin/Release
-dotnet nuget push SlackWriter.1.0.5.nupkg -k $NUGETAPIKEY -s https://api.nuget.org/v3/index.json
+ # go to the output dir for the nuget package and push it to nuget
+ cd /home/vsts/work/1/a &&
+ dotnet nuget push SlackWriter.$(packageVersion).nupkg -k $(NugetApiKey) -s https://api.nuget.org/v3/index.json --skip-duplicate
